159a8b34e746371269212e8ae9f30c0a2516a747
Ralitsa Koseva <ralitsa314@gmail.com>
2025-01-26 20:46:06 +0200

    another one

--- Chat/frontend/chat_front/src/app/app.routes.ts
+++ Chat/frontend/chat_front/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { UserPage } from './features/user/user.component';
+import { ChannelPage } from './features/channel/channel.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path        : '', component : HomeComponent  },
+  { path        : 'users/:id/channels', component   : ChannelPage},
+  { path        : 'users', component   : UserPage }
+];
